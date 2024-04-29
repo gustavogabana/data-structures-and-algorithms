0943f40b87ccc5d309c02dda6f4950cb1581bde2
gustavogabana <gustavo_97@protonmail.com>
2024-04-29 20:11:59 -0300

    feat: new method tail

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -12,7 +12,22 @@ class LinkedList {
         this.tail = this.head;
         this.length = 1;
     }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 let myLinkedList = new LinkedList(4);
+console.log(myLinkedList);
+myLinkedList.push(7);
 console.log(myLinkedList);
