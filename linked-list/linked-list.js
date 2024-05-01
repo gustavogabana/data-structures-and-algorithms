340aa9c5f6dfc175bbbb29fa60facc87371e9f35
gustavogabana <gustavo_97@protonmail.com>
2024-05-01 16:15:19 -0300

    feat: shift method

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -58,6 +58,20 @@ class LinkedList {
         this.length++;
         return this;
     }
+
+    shift(value) {
+        if (!this.head) {
+            return undefined;
+        }
+        let temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return temp;
+    }
 }
 
 let myLinkedList = new LinkedList(4);
